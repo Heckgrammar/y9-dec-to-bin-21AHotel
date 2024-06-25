be1d08e771467282b5678d8642a0bf7a43937463
Alice Hulbert <21AHulbert@heckgrammar.co.uk>
2024-06-25 15:08:26 +0100

    I finally figures it out!!!!!!!!!!!!!!!!!

--- Program.cs
+++ Program.cs
@@ -15,18 +15,46 @@ namespace Y9_DEC_TO_BIN_SKELETON
             string myString = "12"; //watch me being cast from string to int
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
-            //MAIN:  NUMBER CONVERSION PROGRAM
+            //convert integer to string
+            string myIntAsString = Convert.ToString(myInt);
+            Console.WriteLine(myIntAsString);
+            
 
-            //CODE GOES HERE
-        }
+            //Insert a string at the start of another string
+            string myCombinedString = myIntAsString + myString;
+            Console.WriteLine(myCombinedString);
+            
 
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
-        {
-            //CODE GOES HERE
 
-            return result; //REMOVE THE RED LINE!
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.Read();
+
         }
     }
 }
+            //MAIN:  NUMBER CONVERSION PROGRAM
+
+            //CODE GOES HERE
+        //}
+
+        //static void means the function will not return a value so it does not need a data type 
+        //...this function DOES return a value so the method must have a data type
+//        static string numberConversion(int number, int numberbase)
+//        {
+//            //CODE GOES HERE
+
+//            return result; //REMOVE THE RED LINE!
+//        }
+//    }
+//}
