5b8c25b04249913b9d7fd55c3d0577c6587dbf17
Alice Hulbert <21AHulbert@heckgrammar.co.uk>
2024-07-08 09:58:59 +0100

    this should work!!!!!!!!!!!!!!

--- Program.cs
+++ Program.cs
@@ -31,21 +31,26 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //MAIN:  NUMBER CONVERSION PROGRAM
 
             //CODE GOES HERE
-            Console.WriteLine("Please input th estarting denary number");    //prompts the suer to input the number to be converted to binary
+            Console.WriteLine("Please input the starting denary number");    //prompts the suer to input the number to be converted to binary
             string striDenaryNum = Console.ReadLine();  //assigns the variable "denaryNum" as user input
             int denaryNum = Convert.ToInt32(striDenaryNum);    //converts user input to an int
-            int answer = 1;    //assigns the variable "answer" as 1
-            string[] binaryNum = {"", "", "", "", "", "", "", ""};    //creates the array "binaryNum" but it has 8 blank spaces
+            int answer = denaryNum;    //assigns the variable "answer" as 1
+            string[] binaryNum = new string[8];    //creates the array "binaryNum" that is is 8 long
             int index = 0;    //assigns the variable "index" as 0
-            while (answer > -1)    //inniciates a while loop with the condition that the variable "answer" is greater than -1
+            while (answer > 0)    //inniciates a while loop with the condition that the variable "answer" is greater than -1
             {
-                answer = (denaryNum / 2);    //reassigns the variable "answer" as denaryNum divided by 2 but it excludes any remainder
-                int remainder = denaryNum % 2;    //assigns the variable "remainder" as the remainder of denaryNum divided by 2
+                int newAnswer  = (answer / 2);    //assigns the variable "newAnswer" as answer divided by 2 but it excludes any remainder
+                int remainder = answer % 2;    //assigns the variable "remainder" as answer divided by 2 but it excludes any remainder
+                answer = answer / 2;  //assigns the variable "answer" as answer divided by 2 but it excludes any remainder
                 string striRemainder = Convert.ToString(remainder);    //Converts the integer "remainder" to a string and reassigns it at "striRemainder"
-                binaryNum[8-1-index] = striRemainder;    //assigns th
-                index = index++;
+                binaryNum[7-index] = striRemainder;    //assigns the value of one of the positions in the string "binaryNum" as striRemainder
+                index = index++;    //adds 1 to the variable "index"
+            } 
+            if (binaryNum[0] == "")    //checks if the first position in the string "binaryNum" is empty"
+            {
+                binaryNum[0] = "0";    //If it is, it assigns it as "0"
             }
-            Console.WriteLine(binaryNum);
+            Console.WriteLine(binaryNum);    //outputs the string "binaryNum"
             Console.Read();
         }
         //}
