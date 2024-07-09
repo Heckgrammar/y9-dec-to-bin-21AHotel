6efed3194929b80daae6c60c0967d09bc3d4f6ff
Alice Hulbert <21AHulbert@heckgrammar.co.uk>
2024-07-09 15:15:49 +0100

    I FINALLY DID IT YAYAYAYAYAYAYYAYAYAYA

--- Program.cs
+++ Program.cs
@@ -37,31 +37,40 @@ namespace Y9_DEC_TO_BIN_SKELETON
             int answer = denaryNum;    //assigns the variable "answer" as 1
             string[] binaryNum = new string[8];    //creates the array "binaryNum" that is is 8 long
             int index = 0;    //assigns the variable "index" as 0
+            string striRemainder = "";
+            int newAnswer = 0;
+            int remainder = 0;
             while (answer > 0)    //inniciates a while loop with the condition that the variable "answer" is greater than -1
             {
-                int newAnswer  = (answer / 2);    //assigns the variable "newAnswer" as answer divided by 2 but it excludes any remainder
-                int remainder = answer % 2;    //assigns the variable "remainder" as answer divided by 2 but it excludes any remainder
+                newAnswer = (answer / 2);    //assigns the variable "newAnswer" as answer divided by 2 but it excludes any remainder
+                remainder = answer % 2;    //assigns the variable "remainder" as answer divided by 2 but it excludes any remainder
                 answer = answer / 2;  //assigns the variable "answer" as answer divided by 2 but it excludes any remainder
-                string striRemainder = Convert.ToString(remainder);    //Converts the integer "remainder" to a string and reassigns it at "striRemainder"
-                binaryNum[7-index] = striRemainder;    //assigns the value of one of the positions in the string "binaryNum" as striRemainder
-                index = index++;    //adds 1 to the variable "index"
-            } 
-            if (binaryNum[0] == "")    //checks if the first position in the string "binaryNum" is empty"
-            {
-                binaryNum[0] = "0";    //If it is, it assigns it as "0"
+                striRemainder = Convert.ToString(remainder);    //Converts the integer "remainder" to a string and reassigns it at "striRemainder"
+                binaryNum[7 - index] = striRemainder;    //assigns the value of one of the positions in the string "binaryNum" as striRemainder
+                index = index + 1;    //adds 1 to the variable "index"
             }
-            Console.WriteLine(binaryNum);    //outputs the string "binaryNum"
+            Console.WriteLine(binaryNum[0] + binaryNum[1] + binaryNum[2] + binaryNum[3] + binaryNum[4] + binaryNum[5] + binaryNum[6] + binaryNum[7]);    //outputs the string "binaryNum"
             Console.Read();
-        }
-        //}
 
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        //static string numberConversion(int number, int numberbase)
-        //{
-        //    //CODE GOES HERE
-        //    string result = Convert.ToString(number + numberbase);
-        //    return result; //REMOVE THE RED LINE!
+
+            //static string DenToBin(int n, int numbase)  //static string means th edata type being returned is a string 
+            //den to bin is th e identifier of th esubroutine/funtion/method
+            //int n is one of the perametres which is just a variable 
+            //int numbase is antoher parametre
+            //call the subroutine by using its identifier. e.g DenToBin(13,2)
+
+            {
+                //return //value to return goes here
+
+                //static void means the function will not return a value so it does not need a data type 
+                //...this function DOES return a value so the method must have a data type
+                //static string numberConversion(int number, int numberbase)
+                //{
+                //    //CODE GOES HERE
+                //    string result = Convert.ToString(number + numberbase);
+                //    return result; //REMOVE THE RED LINE!
+            }
         }
-   }
+    }
+}
 
