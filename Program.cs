85a81ec1e2ca569a7f2b861b3acc5f9942287897
Alice Hulbert <21AHulbert@heckgrammar.co.uk>
2024-07-17 14:15:01 +0100

    i hope i can fix this at some point

--- Program.cs
+++ Program.cs
@@ -31,32 +31,56 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //MAIN:  NUMBER CONVERSION PROGRAM
 
             //CODE GOES HERE
-        }
-            private static int DenaryToBinary(int denary)
+
+
+
+            //Console.WriteLine("Please input the starting denary number");    //prompts the suer to input the number to be converted to binary
+            //int denaryNum = Convert.ToInt32(Console.ReadLine());    //converts user input to an int
+            //int answer = denaryNum;    //assigns the variable "answer" as denary num
+            //string[] binaryNum = new string[8];    //creates the array "binaryNum" that is is 8 long
+            //int index = 0;    //assigns the variable "index" as 0
+            //string striRemainder = "";
+            //int newAnswer = 0;
+            //int remainder = 0;
+            //while (answer > 0)    //inniciates a while loop with the condition that the variable "answer" is greater than -1
+            //{
+            //    newAnswer = (answer / 2);    //assigns the variable "newAnswer" as answer divided by 2 but it excludes any remainder
+            //    remainder = answer % 2;    //assigns the variable "remainder" as answer divided by 2 but it excludes any remainder
+            //    answer = answer / 2;  //assigns the variable "answer" as answer divided by 2 but it excludes any remainder
+            //    striRemainder = Convert.ToString(remainder);    //Converts the integer "remainder" to a string and reassigns it at "striRemainder"
+            //    binaryNum[7 - index] = striRemainder;    //assigns the value of one of the positions in the string "binaryNum" as striRemainder
+            //    index = index + 1;    //adds 1 to the variable "index"
+            //}
+            //Console.WriteLine(binaryNum[0] + binaryNum[1] + binaryNum[2] + binaryNum[3] + binaryNum[4] + binaryNum[5] + binaryNum[6] + binaryNum[7]);    //outputs the string "binaryNum"
+            //Console.Read();
+
+            //Extension: Convert binary to denary
+
+            Console.WriteLine("Please input your binary number with no spaces: ");
+            string binaryNumber = (Console.ReadLine());
+            int length = binaryNumber.Length - 1;
+            int value = 0;
+            int collumn = 0;
+            for (int i = 0; i <= length; i++)
             {
-                Console.WriteLine("Please input the starting denary number");    //prompts the suer to input the number to be converted to binary
-                string striDenaryNum = Console.ReadLine();  //assigns the variable "denaryNum" as user input
-                int denaryNum = Convert.ToInt32(striDenaryNum);    //converts user input to an int
-                int answer = denaryNum;    //assigns the variable "answer" as 1
-                string[] binaryNum = new string[8];    //creates the array "binaryNum" that is is 8 long
-                int index = 0;    //assigns the variable "index" as 0
-                string striRemainder = "";
-                int newAnswer = 0;
-                int remainder = 0;
-                while (answer > 0)    //inniciates a while loop with the condition that the variable "answer" is greater than -1
+                string singleDigit = binaryNumber.Substring(length - i, 1);
+                if (i == 0)
                 {
-                    newAnswer = (answer / 2);    //assigns the variable "newAnswer" as answer divided by 2 but it excludes any remainder
-                    remainder = answer % 2;    //assigns the variable "remainder" as answer divided by 2 but it excludes any remainder
-                    answer = answer / 2;  //assigns the variable "answer" as answer divided by 2 but it excludes any remainder
-                    striRemainder = Convert.ToString(remainder);    //Converts the integer "remainder" to a string and reassigns it at "striRemainder"
-                    binaryNum[7 - index] = striRemainder;    //assigns the value of one of the positions in the string "binaryNum" as striRemainder
-                    index = index + 1;    //adds 1 to the variable "index"
+                    collumn = i * 2;
                 }
-                Console.WriteLine(binaryNum[0] + binaryNum[1] + binaryNum[2] + binaryNum[3] + binaryNum[4] + binaryNum[5] + binaryNum[6] + binaryNum[7]);    //outputs the string "binaryNum"
-                Console.Read();
-
+                else
+                {
+                    collumn = 1;
+                }
+                if (singleDigit == "1")
+                {
+                    value = value + collumn;
+                }
             }
+            Console.WriteLine(value);
 
+
+            Console.Read();
 
             //static string DenToBin(int n, int numbase)  //static string means thE data type being returned is a string 
             //den to bin is th e identifier of th esubroutine/funtion/method
@@ -64,16 +88,17 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //int numbase is antoher parametre
             //call the subroutine by using its identifier. e.g DenToBin(13,2)
 
-            
-                //return //value to return goes here
 
-                //static void means the function will not return a value so it does not need a data type 
-                //...this function DOES return a value so the method must have a data type
-                //static string numberConversion(int number, int numberbase)
-                //{
-                //    //CODE GOES HERE
-                //    string result = Convert.ToString(number + numberbase);
-                //    return result; //REMOVE THE RED LINE!
+            //return //value to return goes here
+
+            //static void means the function will not return a value so it does not need a data type 
+            //...this function DOES return a value so the method must have a data type
+            //static string numberConversion(int number, int numberbase)
+            //{
+            //    //CODE GOES HERE
+            //    string result = Convert.ToString(number + numberbase);
+            //    return result; //REMOVE THE RED LINE!
+        }
     }
 }
     
